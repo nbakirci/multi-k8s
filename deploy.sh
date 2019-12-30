ea63e0ea3a9fbc74068f4ff2b5a4f01068f44a68
Nareg <nareg@Nathans-MacBook-Pro.local>
2019-12-30 16:03:27 -0500

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t naregb/multi-client:latest -t naregb/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t naregb/multi-server:latest -t naregb/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t naregb/multi-worker:latest -t naregb/multi-worker:$SHA -f ./wokrer/Dockerfile ./worker
+docker push naregb/multi-client:latest
+docker push naregb/multi-server:latest
+docker push naregb/multi-worker:latest
+
+docker push naregb/multi-client:latest:$SHA
+docker push naregb/multi-server:latest:$SHA
+docker push naregb/multi-worker:latest:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=naregb/multi-server:$SHA
+kubectl set image deplyoments/client-deployment client=naregb/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=naregb/multi-worker:$SHA
